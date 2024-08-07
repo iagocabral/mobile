9413721fa70cfe2feb722df73b9a4b0ccc282feb
Iago Cabral <iagocabral.software@gmail.com>
2024-08-06 23:06:13 -0300

    PROJETO PAUSADO- start auth UI

--- pizza_app/lib/main.dart
+++ pizza_app/lib/main.dart
@@ -1,5 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pizza_app/app.dart';
+import 'package:user_repository/user_repository.dart';
+import 'simple_bloc_observer.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
+  runApp (MyApp(FirebaseUserRepo()));
 }
