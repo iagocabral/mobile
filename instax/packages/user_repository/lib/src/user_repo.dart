271ecb17ac717bd1815b57af457e50d93fa41b61
Iago Cabral <iagocabral.software@gmail.com>
2024-08-15 17:17:56 -0300

    UserRepository  class
    Criando e implementando os metodos da classe UserRepository

--- instax/packages/user_repository/lib/src/user_repo.dart
+++ instax/packages/user_repository/lib/src/user_repo.dart
@@ -1,3 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/user_repository.dart';
+
 abstract class UserRepository {
+
+  Stream<User?> get user;
+
+  Future<void> signIn(String email, String password);
+
+  Future<void> logOut();
+
+  Future<MyUser> signUp(MyUser myUser, String password);
+
+  Future<void> resetPassword(String email); 
+
+  Future<void> setUserData(MyUser myUser);
+
+  Future<MyUser> getUserData(String myUserId);
 
 }
