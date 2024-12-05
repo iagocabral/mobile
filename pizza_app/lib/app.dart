135e874b835ff3ecd8593bb14a65203503366f16
Iago Cabral <iagocabral.software@gmail.com>
2024-12-05 16:54:22 -0300

    Auth Screen/BloC

--- pizza_app/lib/app.dart
+++ pizza_app/lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_repository/user_repository.dart';
-import 'blocs/bloc/authentication_bloc.dart';
+import 'blocs/authentication_bloc/authentication_bloc.dart';
 
 import 'app_view.dart';
 
